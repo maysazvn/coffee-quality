8f6f958d72d2f5dc992453add5e28ccceb266c71
maysazvn <maysabmachado84@gmail.com>
2026-07-14 15:44:15 -0300

    alguns ajustes

--- src/views/AvaliacoesView.vue
+++ src/views/AvaliacoesView.vue
@@ -7,11 +7,40 @@ import { cafesAvaliados } from '@/data/CafesAvaliados';
     <h1>Avaliações de cafés</h1>
     <p>blablabla</p>
 
-    <div v-for="cafes in cafesAvaliados">
+    <div v-for="cafes in cafesAvaliados" :key="cafes.id" class="avaliadosCards">
+        <div>
+        <h2>{{ cafes.nome }}</h2>
+        <p class="produtor">Produtor: {{ cafes.produtor }}</p>
+        <p class="avaliado">Avaliado em {{ cafes.dtavaliacao }}</p>
+        </div>
 
+        <div>
+            <p class="media">Média SCA</p>
+            <p class="numero">{{ cafes.media }}</p>
+        </div>
     </div>
 </section>
 </template>
 
 <style scoped>
+
+.avaliados{
+margin: 2vw;
+display: flex;
+flex-direction: column;
+}
+
+.avaliadosCards{
+display: flex;
+justify-content: space-between;
+border: 1px solid #b1b1b1;
+border-radius: 15px;
+padding: 15px;
+margin-top: 5px;
+}
+
+h2{
+    font-weight: bold;
+    font-size: 1.2rem;
+}
 </style>
